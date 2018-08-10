ee552a4ebabe034c39da03949aaba7e019b2b873
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-08-10 10:56:32 +0200

    bug fix

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ var fs = require('fs');
 
 var server = http.createServer();
 
-server.on('request', function (request, response){
-	response.setHeader("Content-Type", "text/html; charset=utf-8");
-	if(request.method === "GET" && request.url === '/hello') {
-		response.write('<h1>Hello World!</h1>');
-			response.end();
-	} else {
-		response.statusCode = 404;
-		response.write('<h1>404: Wrong way </h1>');
-		fs.readFile('./tree.jpg');
-		response.end();
-	}
 
+server.on('request', function (request, response) {
+  response.setHeader("Content-Type", "text/html; charset=utf-8");
+  if (request.method === 'GET' && request.url === '/quijote') {
+    fs.readFile('./index.html', 'utf-8', function (err, file) {
+      if (err) {
+        throw err;
+      } else {
+        response.write(file);
+      }
+    });
+  } else {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    response.statusCode = 404;
+    fs.readFile("./tree.jpg", function (err, img) {
+      if (err) {
+        throw err;
+      } else {
 
+        response.write('<h1>Wrong way</h1><img src="data:image/jpg;base64, '+Buffer.from(img).toString("base64")+'">');
+        response.end();
+      };
+    });
+  }
 
 });
-
 server.listen(8000);
